8cfff06c8e08d2b0b7b9633dea63401798a2580e
lexmin0412 <zhangle_media@hotmail.com>
2021-12-26 00:51:03 +0800

    chore: 添加base配置

--- .umirc.ts
+++ .umirc.ts
@@ -7,5 +7,6 @@ export default defineConfig({
   logo: 'https://user-images.githubusercontent.com/9554297/83762004-a0761b00-a6a9-11ea-83b4-9c8ff721d4b8.png',
   outputPath: 'docs-dist',
   mode: 'site',
+  base: process.env.NODE_ENV === 'production' ? '/react-dnd-anything/' : '/',
   publicPath: process.env.NODE_ENV === 'production' ? '/react-dnd-anything/' : '/',
 });
