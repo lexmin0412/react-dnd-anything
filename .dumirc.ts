624cdc69ea966e6ae7ac24c9a3e7e0b42bef8a01
lexmin0412 <zhangle_dev@outlook.com>
2023-04-17 23:00:23 +0800

    chore(dumi theme): custom footer and social links

--- .dumirc.ts
+++ .dumirc.ts
@@ -2,9 +2,13 @@ import { defineConfig } from 'dumi';
 
 export default defineConfig({
   base: '/react-dnd-anything/',
+  publicPath: '/react-dnd-anything/',
   outputPath: 'docs-dist',
   themeConfig: {
     name: 'RDA',
+    footer: `<div>Copyright © 2023 | Powered by <a href="https://d.umijs.org/guide" target="_blank">dumi</a> | Authored by <a href="https://github.com/lexmin0412" target="_blank">Lexmin0412</a>.</div>`,
+    socialLinks: {
+      github: 'https://github.com/lexmin0412/react-dnd-anything',
+    },
   },
-  publicPath: '/react-dnd-anything/',
 });
